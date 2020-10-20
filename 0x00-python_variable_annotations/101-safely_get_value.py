dbbe2791e248bbb0a9e8191e9a2667ece93d1169
andres-martin <andresmartin266@gmail.com>
2020-10-19 20:08:49 -0500

    [add] type annotations python3 task 101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""iterable object"""
+"""iterable object TypeVar"""
 from typing import Any, Union, Mapping, Optional, TypeVar
 
 T = TypeVar('T')
@@ -7,7 +7,7 @@ T = TypeVar('T')
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
-    """returns map key value"""
+    """returns mapped key value"""
     if key in dct:
         return dct[key]
     else:
