a3508bae2a2b410f75662f0ce9dbd3ceabb5ec20
andres-martin <andresmartin266@gmail.com>
2020-10-20 23:30:33 -0500

    [add] retuns a list of delayed tasks asyncio

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""Python and Async"""
+"""Python and Async task await random"""
 import asyncio
 from typing import List
 
-t = __import__('3-tasks').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """returns list of done async tasks"""
-    tasks = [t(max_delay) for _ in range(n)]
-    done_tasks = [await task for task in asyncio.as_completed(tasks)]
-    return done_tasks
+    """returns list of delayed async tasks"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delayed_tasks = [await task for task in asyncio.as_completed(tasks)]
+    return delayed_tasks
