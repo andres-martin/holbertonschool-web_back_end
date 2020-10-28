8af5bdd197d8993ac09e00791f1104f075cc4332
Martin <andresmartin266@gmail.com>
2020-10-28 02:48:37 +0000

    [wip] 2 hyper pagination

---
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+''' self descriptive  '''
+import csv
+import math
+from typing import Tuple, List, Dict
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''self descriptive'''
+    return (((page - 1) * page_size), (page * page_size))
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''self descriptive'''
+        assert isinstance(page, int), "page should be integer"
+        assert isinstance(page_size, int), "page_size should be integer"
+        assert page > 0, "page should be greater than 0"
+        assert page_size > 0, "page_size should be greater than 0"
+
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, any]:
+        '''self descriptive'''
+        data = self.get_page(page, page_size)
+        total_pages = len(self.dataset())
