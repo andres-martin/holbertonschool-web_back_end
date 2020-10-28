f83abe5f2c61b7aa59e0cb7ad8d8cf1f3a9623eb
Martin <andresmartin266@gmail.com>
2020-10-28 16:53:30 +0000

    [add] hyper media pagination

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -42,4 +42,15 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, any]:
         '''self descriptive'''
         data = self.get_page(page, page_size)
-        total_pages = len(self.dataset())
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        data = {'page_size': len(data),
+                'page': page,
+                'data': data,
+                'next_page': next_page,
+                'prev_page': prev_page,
+                'total_pages': total_pages,
+                }
+        return data
