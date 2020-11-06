b2337efaa9cf3e82117264a7af9a8ecf1306a6ba
andres-martin <andresmartin266@gmail.com>
2020-11-05 22:49:19 -0500

    [add] endoding base64

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 from typing import List, TypeVar
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -15,3 +17,15 @@ class BasicAuth(Auth):
                 authorization_header,
                 str) and authorization_header.startswith("Basic "):
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_auth_header: str) -> str:
+        '''self descriptive'''
+        if base64_auth_header and isinstance(
+                base64_auth_header, str):
+            try:
+                encoded = base64_auth_header.encode('utf-8')
+                base = base64.b64decode(encoded)
+                return base.decode('utf-8')
+            except binascii.Error:
+                return None
