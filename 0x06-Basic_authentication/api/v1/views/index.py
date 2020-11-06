9742d049c29ea1b7ab8b272961c35135bfc18db5
andres-martin <andresmartin266@gmail.com>
2020-11-05 21:23:01 -0500

    [add] unauthorized route

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raises an error
+    """
+    abort(401, description='Unauthorized')
