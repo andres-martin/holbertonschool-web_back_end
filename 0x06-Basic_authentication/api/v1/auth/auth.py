853149fd9cea50fb73ca8c1594d198939fa2a2ed
andres-martin <andresmartin266@gmail.com>
2020-11-05 22:03:12 -0500

    [add] task 4 excluded paths

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,14 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''self descriptive'''
-        return False
+        if path is None or excluded_paths is None:
+            return True
+
+        path += '/' if path[-1] != '/' else ''
+
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''self descriptive'''
