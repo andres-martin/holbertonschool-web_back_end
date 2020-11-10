286b152afa70b21ac1a879972602f1d4ee676af3
andres-martin <andresmartin266@gmail.com>
2020-11-10 18:57:58 -0500

    [add] session authentication

---
+++ 0x07-Session_authentication/api/v1/app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    '''self descriptive'''
+    blacklist = ['/api/v1/status/', '/api/v1/unauthorized/',
+                 '/api/v1/forbidden/']
+
+    if auth and auth.require_auth(request.path, blacklist):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+    request.current_user = auth.current_user(request)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" auth module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''self descriptive'''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''self descriptive'''
+        if not path or not excluded_paths:
+            return True
+
+        path += '/' if path[-1] != '/' else ''
+        wildcard = any(rex.endswith("*") for rex in excluded_paths)
+
+        if not wildcard:
+            if path in excluded_paths:
+                return False
+
+        for rex in excluded_paths:
+            if rex[-1] == '*':
+                if path.startswith(rex[:-1]):
+                    return False
+            if rex == path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        '''self descriptive'''
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''self descriptive'''
+        return None
