c28f59536d613074ca4baa3102888ab453c0bace
andres-martin <andresmartin266@gmail.com>
2020-11-10 20:33:32 -0500

    [add] expiration auth

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 import os
 
 
@@ -23,6 +24,8 @@ elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
 elif getenv("AUTH_TYPE") == "session_auth":
     auth = SessionAuth()
+elif getenv("AUTH_TYPE") == "session_exp_auth":
+    auth = SessionExpAuth()
 
 
 @app.errorhandler(404)
