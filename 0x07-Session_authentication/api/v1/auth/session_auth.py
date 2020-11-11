fa449e3c486997517324996878a16412704a8432
andres-martin <andresmartin266@gmail.com>
2020-11-10 19:20:09 -0500

    [add] sesiin auth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -11,4 +11,12 @@ from models.user import User
 
 class SessionAuth(Auth):
     '''self descriptive'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''self descriptive'''
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
