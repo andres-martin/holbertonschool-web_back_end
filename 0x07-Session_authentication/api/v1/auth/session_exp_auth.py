fbb9cd6ab230103c32357dad3c7aef7fea44f7a7
andres-martin <andresmartin266@gmail.com>
2020-11-10 20:35:32 -0500

    [add] expiration auth

--- 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -49,7 +49,7 @@ class SessionExpAuth(SessionAuth):
                 created_at = session_dictionary.get('created_at')
                 if created_at is None:
                     return None
-                if created_at + timedelta(seconds=self.session_duration) <
-                        datetime.now():
+                if (created_at + timedelta(seconds=self.session_duration) <
+                        datetime.now()):
                     return None
                 return user
