244c34f4dd43260d661adb7ad42796a0b539c25b
andres-martin <andresmartin266@gmail.com>
2020-11-16 18:14:13 -0500

    [add] resgiter users endpoint

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -5,12 +5,26 @@ from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def root() -> str:
     ''' root route '''
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users(email: str, password: str) -> str:
+    ''' sefl descriptive '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({'email': email, 'message': 'user created'})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
