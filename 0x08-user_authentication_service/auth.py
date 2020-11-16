87bca4693562b05c4cae2fc35888b646df8f8e6e
andres-martin <andresmartin266@gmail.com>
2020-11-16 18:48:25 -0500

    [add] find user by session id

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -55,10 +55,10 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-     def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+     def get_user_from_session_id(self, session_id: str) -> str:
         """ get user from session id
         """
         try:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
-            return None
+            return
