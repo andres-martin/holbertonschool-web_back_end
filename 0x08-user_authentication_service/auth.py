58919f3fc4bc26db7291a88e1dea47763b3d1c05
andres-martin <andresmartin266@gmail.com>
2020-11-16 18:02:21 -0500

    [add] register user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -12,3 +12,21 @@ def _hash_password(password: str) -> str:
     '''self descriptive'''
     salted_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return salted_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        ''' self descriptive '''
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            new_registry = self._db.add_user(email, _hash_password(password))
+            return new_registry
+        else:
+            raise ValueError(f'User {email} already exists')
