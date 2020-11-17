b731527534a46cc67798c283f8d65b12a50a9de3
andres-martin <andresmartin266@gmail.com>
2020-11-16 19:27:00 -0500

    [add] reset token

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -66,3 +66,16 @@ class Auth:
         ''' self descriptive '''
         if user_id:
             self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """reset password token if user exists"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        token = _generate_uuid()
+
+        self._db.update_user(user.id, reset_token=token)
+
+        return reset_token
