0aa440fc6e3c84aa58f72a0b651a9bac317c9517
andres-martin <andresmartin266@gmail.com>
2020-11-16 19:34:17 -0500

    [add] update password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -82,12 +82,13 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """ self descriptive update password """
-        if reset_token and password:
-            try:
-                user = self._db.find_user_by(email=email)
-            except NoResultFound:
-                raise ValueError
-            else:
-                hashed_pwd = _hash_password(password)
-                self._db.update_user(
-                    user.id, password=hashed_pwd, reset_token=None)
+        if not reset_token or not password:
+            return None
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            hashed_pwd = _hash_password(password)
+            self._db.update_user(
+                user.id, password=hashed_pwd, reset_token=None)
