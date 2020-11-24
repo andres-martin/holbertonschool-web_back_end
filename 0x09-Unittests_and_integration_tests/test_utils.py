66db8c8747040fb5aacfb05c55c038e73c3201ee
andres-martin <andresmartin266@gmail.com>
2020-11-23 20:43:18 -0500

    [add] parameterized test KeyError

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         ''' test access nested map '''
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        ''' test exception'''
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
