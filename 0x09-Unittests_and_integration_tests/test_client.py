16f70c1f80cbdf4c0a1af6f56d0c398bec503afb
andres-martin <andresmartin266@gmail.com>
2020-11-23 22:34:09 -0500

    [add] parameterized and patch as decorators

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" Unit test
+""" Unit test Test client
 """
 import unittest
+import json
 from parameterized import parameterized
 from unittest import mock
 from unittest.mock import patch, Mock
-import client
+from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -19,6 +20,6 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_org(self, data, mock):
         ''' self descriptive '''
         endpoint = f'https://api.github.com/orgs/{data}'
-        spec = GithubOrgClient(url)
+        spec = GithubOrgClient(data)
         spec.org()
         mock.assert_called_once_with(endpoint)
