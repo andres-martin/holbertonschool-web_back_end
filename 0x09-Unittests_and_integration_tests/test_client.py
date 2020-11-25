7e74f563305545b3eaca267d779fb5172e133a10
andres-martin <andresmartin266@gmail.com>
2020-11-24 21:15:42 -0500

    [add] mocking a property

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -3,9 +3,9 @@
 """
 import unittest
 import json
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from unittest import mock
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
 
 
@@ -19,7 +19,16 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_org(self, data, mock):
         ''' self descriptive '''
-        endpoint = f'https://api.github.com/orgs/{data}'
+        endpoint = "f'https://api.github.com/orgs/{data}'"
         spec = GithubOrgClient(data)
         spec.org()
         mock.assert_called_once_with(endpoint)
+
+    @parameterized.expand([
+        ("random-url", {'repos_url': 'http://some_url.com'})
+    ])
+    def test_public_repos_url(self, name, result):
+        '''self descriptive'''
+        with patch('client.GithubOrgClient.org', PropertyMock(return_value=result)):
+            response = client.GithubOrgClient(name)._public_repos_url
+            self.assertEqual(response, result.get('repos_url'))
