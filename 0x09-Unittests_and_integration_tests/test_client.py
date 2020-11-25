bcad3e98a0d22cf13931f2f14ef66d5df4b59d17
andres-martin <andresmartin266@gmail.com>
2020-11-24 21:28:42 -0500

    [add] mocking a property

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -33,3 +33,15 @@ class TestGithubOrgClient(unittest.TestCase):
                    PropertyMock(return_value=result)):
             response = client.GithubOrgClient(name)._public_repos_url
             self.assertEqual(response, result.get('repos_url'))
+
+    @patch('client.get_json')
+    def test_public_repos(self, mocked_method):
+        '''self descriptive'''
+        mocked_method.return_value = [{"name": "a"}, {"name", "b"}]
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as mocked_guy:
+            mocked_guy.return_value = 'a'
+            response = GithubOrgClient('a').public_repos()
+            self.assertEqual(response, ['a', 'b'])
+            mocked_method.assert_called_once()
+            mocked_guy.assert_called_once()
