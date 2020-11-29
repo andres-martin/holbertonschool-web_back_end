93c0fe5daf1681ba9c20617ee679e159b04dd128
andres-martin <andresmartin266@gmail.com>
2020-11-28 20:12:23 -0500

    [add] get locale from request

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Flask app
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
