a455840228470fa1261848b3aba1efd8e4f6ade6
andres-martin <andresmartin266@gmail.com>
2020-11-28 21:23:05 -0500

    [add] logged in message

---
+++ 0x0A-i18n/5-app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" Basic Flask app
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    ''' babel config class '''
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+@babel.localeselector
+def get_locale():
+    ''' get locale from request '''
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+@babel.localeselector
+def get_user():
+    ''' returns user from mocked db '''
+    login_as = request.args.get("login_as", False)
+    if login_as:
+        user = users.get(int(login_as), False)
+    if user:
+        return user
+    return None
+
+@app.before_request
+def before_request():
+    ''' self descriptive '''
+    g.user = get_user()
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """ Returns index """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
