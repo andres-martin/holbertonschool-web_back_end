f188bd2dab4c9b2a4cada13b9d22a3e51362b44c
andres-martin <andresmartin266@gmail.com>
2020-11-30 21:16:55 -0500

    [add] get redis cache function

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,9 @@
 ''' Redis server '''
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
+
+UnionOfTypes = Union[str, bytes, int, float]
 
 
 class Cache:
@@ -13,9 +15,18 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: UnionOfTypes) -> str:
         '''store to redis the input data'''
         random_key = str(uuid4())
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable]) -> UnionOfTypes:
+        '''get method to retrive value from redis cache'''
+        value = self._redis.get(key)
+
+        if fn:
+            value = fn(value)
+
+        return value
