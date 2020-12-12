f461f14e4e5bb08858e2baf5db665ec51d2c5494
andres-martin <andresmartin266@gmail.com>
2020-12-11 21:39:46 -0500

    [add] ES6 basics

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,8 @@
-/* eslint-disable no-plusplus */
 export default function createIteratorObject(report) {
-  const employees = Object.values(report.allEmployees);
-  let nextIndex = 0;
-  return {
-    next() {
-      return nextIndex < employees.length
-        ? { value: employees[nextIndex++], done: false }
-        : { done: true };
-    },
-  };
+  let result = [];
+  for (const value of Object.values(report.allEmployees)) {
+    result = [...result, ...value];
+  }
+
+  return result;
 }
