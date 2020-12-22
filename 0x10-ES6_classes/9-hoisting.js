4d17317e0cba010f74f6a7b9a39bf89ab5903300
andres-martin <andresmartin266@gmail.com>
2020-12-21 20:57:19 -0500

    [add] tasks ES6

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -1,3 +1,4 @@
+
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -12,12 +13,14 @@ export class HolbertonClass {
     return this._location;
   }
 }
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = HolbertonClass;
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -25,19 +28,14 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 }
-
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
 const listOfStudents = [student1, student2, student3, student4, student5];
 
-module.exports = listOfStudents;
+export default listOfStudents;
