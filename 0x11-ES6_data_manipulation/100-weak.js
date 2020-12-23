20d8c964fc3f6988c701d664676956851fa09e91
andres-martin <andresmartin266@gmail.com>
2020-12-23 18:28:01 -0500

    [add] data manipulation

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -2,8 +2,8 @@ const weakMap = new WeakMap();
 
 const queryAPI = (endpoint) => {
   const total = weakMap.get(endpoint) || 0;
-  if (total >= 5) throw new Error('Endpoint load is high');
   weakMap.set(endpoint, total + 1);
+  if (total >= 5) throw new Error('Endpoint load is high');
 };
 
 export { weakMap, queryAPI };
